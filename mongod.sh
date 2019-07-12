b0e6e6bf33f752951a29bef04e340146058cc7b5
d.w.oneill.18 <d.w.oneill.18@gmail.com>
2019-07-12 14:32:41 +0000

    update

--- mongod.sh
+++ mongod.sh
@@ -8,12 +8,13 @@ sudo apt -y install git
 #sudo apt install screen
 sudo apt -y install curl
 sudo apt install -y mongodb
-sudo apt -y update
 mongod --version
 
 
 #setup db
 cd Mongo/
-sudo cp mongodb.conf /etc/
 sudo systemctl start mongodb
 sudo systemctl status mongodb
+echo "finished"
+echo "finished again"
+
